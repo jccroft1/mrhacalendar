1bd9ece0ded5a1fc0c142bc3a83db4208eb8fe72
Jack Croft <jc.croft1@gmail.com>
2020-03-01 11:29:49 +0000

    convert to HTTPS using autocert

--- httpserver.go
+++ httpserver.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+var (
+	domain string
+	port   string
 )
 
 func runHTTPServer() {
+	flag.StringVar(&domain, "domain", "www.example.com", "domain whitelist for configuring autocert")
+	flag.StringVar(&port, "port", ":443", "custom port for server to listen on")
+	flag.Parse()
+
 	http.HandleFunc("/calendar/mrha/", handleMrhaCalendar)
-	log.Println("listening on 59463")
-	panic(http.ListenAndServe(":59463", nil))
+
+	manager := &autocert.Manager{
+		Cache:      autocert.DirCache("secrets"),
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(domain),
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/calendar/mrha/", handleMrhaCalendar)
+
+	server := &http.Server{
+		Addr:         port,
+		TLSConfig:    manager.TLSConfig(),
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	log.Println("listening on", port)
+	log.Fatal(server.ListenAndServeTLS("", ""))
 }
 
 func handleMrhaCalendar(w http.ResponseWriter, r *http.Request) {
